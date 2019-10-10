8c2a3af441631b74184e12ef27fae861e5a41f6c
raptor <goatman.py@gmail.com>
2019-10-11 05:57:40 +0900

    pythonic++ syntax, auto convert "python-style-c++" into actual c++ https://gitlab.com/hartsantler/tpythonpp/issues/13

---
+++ benchmarks/mandelbrot_pythonic++.py
@@ -0,0 +1,40 @@
+with c++:
+	@module( mycppmodule )
+	def mandelbrot():
+		const double h = 150.0
+		double Z = 0.0; double z = 0.0; double T = 0.0; double t = 0.0; double C = 0.0
+		double c = 0.0; double U = 0.0; double V = 0.0; double K = 1.5; double k = 1.0
+		double y = 0
+		while y < 150:
+			y ++
+			double x = 0
+			while x < 150:
+				x ++
+				Z=0.0; z=0.0; T=0.0; t = 0.0
+				U = x*2
+				U /= h
+				V = y*2
+				V /= h
+				C = U - K
+				c = V - k
+				double i = 0
+				while i < 50:
+					i ++
+					if T+t <= 4:
+						z = Z*z
+						z *= 2
+						z += c
+						Z = T - t
+						Z += C
+						T = Z * Z
+						t = z * z
+				if T+t <= 4:
+					std::cout << '*'
+				else:
+					std::cout << '.'
+		return tp_None;
+
+
+import mycppmodule
+mycppmodule.mandelbrot()
+
--- tpython++compiler.py
+++ tpython++compiler.py
@@ -2,6 +2,106 @@
 # -*- coding: utf-8 -*-
 import os, sys, subprocess
 
+def pythonicpp( source ):
+	out = []
+	prev = None
+	prevs = None
+	previ = None
+	autobrace = 0
+	autofunc = 0
+	mods = {}
+	modname = None
+	for ln in source:
+		indent = 0
+		for c in ln:
+			if c == '\t':
+				indent += 1
+
+		if indent < previ and autobrace:
+			braces = previ - indent
+			b = '\t' * indent
+			b += '}'*braces
+			out.append(b)
+
+		s = ln.strip()
+
+		if s.startswith('@'):
+			assert s.startswith('@module')
+			assert s.count('(')==1
+			assert s.count(')')==1
+			modname = s.split('(')[-1].split(')')[0]
+			if modname not in mods:
+				mods[modname] = []
+			out.append('// module: ' + modname)
+		elif s.startswith('def '):
+			assert s.endswith(':')
+			autobrace += 1
+			autofunc += 1
+			func_name = s[len('def ') : ].split('(')[0]
+			if '->' in s:
+				returns = s[:-1].split('->')[-1]
+			elif prevs.startswith('@module'):
+				returns = 'tp_obj'
+			else:
+				returns = 'void'
+
+			args = []
+			rawargs = s.split('(')[-1].split(')')[0]
+			for i, arg in enumerate(rawargs.split(',')):
+				if arg == 'TP':
+					if i != 0:
+						raise SyntaxError('ERROR: `TP` must be the first argument')
+				elif arg and ' ' not in arg:
+					arg = 'auto ' + arg
+
+				if arg:
+					args.append( arg )
+
+			if prevs.startswith('@module'):
+				if not len(args):
+					args.append('TP')
+				elif args[0] != 'TP':
+					args.insert(0, 'TP')
+
+			#func = '\t' * indent
+			func = '%s %s(%s) {' %(returns, func_name, ','.join(args))
+			out.append(func)
+
+		elif s.startswith('while ') and s.endswith(':'):
+			autobrace += 1
+			w = '\t' * indent
+			w += 'while(' + s[len('while '):-1] + ') {'
+			out.append(w)
+
+		elif s.startswith('if ') and s.endswith(':'):
+			autobrace += 1
+			w = '\t' * indent
+			w += 'if(' + s[len('if '):-1] + ') {'
+			out.append(w)
+
+		elif s == 'else:':
+			autobrace += 1
+			w = '\t' * indent
+			w += 'else {'
+			out.append(w)
+		else:
+			if not s.endswith( ('{', '}') ):
+				if not s.endswith(';'):
+					ln += ';'
+			out.append(ln)
+
+		prev = ln
+		prevs = s
+		previ = indent
+
+	if autofunc:
+		out.append('}')
+
+	cpp = '\n'.join(out)
+	if '--inspect-pythonic++' in sys.argv:
+		raise RuntimeError(cpp)
+	return cpp
+
 def metapy2tinypypp( source ):
 	shared = []
 	right_side = []
@@ -58,7 +158,8 @@ def metapy2tinypypp( source ):
 		script = '\n'.join(shared)
 		scripts.append(script)
 
-	return scripts, '\n'.join(cpp)
+	cpp = pythonicpp( cpp )
+	return scripts, cpp
 
 def main():
 	input_file = None
