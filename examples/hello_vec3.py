69c899a1bf1f37d17e111c780f183e3951ea452e
raptor <goatman.py@gmail.com>
2019-10-28 18:38:00 +0900

    vector3 hello world https://gitlab.com/hartsantler/tpythonpp/issues/17

--- examples/hello_vec3.py
+++ examples/hello_vec3.py
@@ -1,19 +1,17 @@
 def test():
 	print('hello world')
-	v = vec2(1.1, 2.2)
+	v = vec3(1.1, 2.2, 3.3)
 	print(v)
 	print(v.x)
 	print(v.y)
+	print(v.z)
 	v.x = 99
 	print(v.x)
 	v.y += 400
 	print(v.y)
-	ratio = v.aspect()
-	print(ratio)
 	print(v.length())
 	v2 = v.normalized()
 	print(v2)
-	print(v2.angle())
 	v3 = v + v2
 	print(v3)
 
