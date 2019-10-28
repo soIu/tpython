931ff952708018120bdf6f4c2ab176fff3800d6b
raptor <goatman.py@gmail.com>
2019-10-28 13:53:25 +0900

    set x and y for vec2 type. note that setting x or y will not work for a vec2 that is global, because tp_set has been changed to return self and set RA, and globals are stored elsewhere.

--- examples/hello_vec2.py
+++ examples/hello_vec2.py
@@ -1,5 +1,12 @@
-print('hello world')
-v = vec2(1.1, 2.2)
-print(v)
-print(v.x)
-print(v.y)
+def test():
+	print('hello world')
+	v = vec2(1.1, 2.2)
+	print(v)
+	print(v.x)
+	print(v.y)
+	v.x = 99
+	print(v.x)
+	v.y += 400
+	print(v.y)
+
+test()
