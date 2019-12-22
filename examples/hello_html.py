395893c4dbeeb7324d6b3ff76c1f7bb491b5c6f9
raptor <goatman.py@gmail.com>
2019-12-22 09:18:23 +0900

    new builtin `javascript(code, returns=type)` allows simple inline javascript that can capture the return value of the expression, valid types are: 'void', 'int', 'float', and 'string'

--- examples/hello_html.py
+++ examples/hello_html.py
@@ -5,4 +5,14 @@ with javascript:
 
 print('hi from tpy interpreter')
 
+javascript("console.log('calling js from tpy')", returns='void')
+
+x = javascript("1+1", returns='int')
+print(x)
+
+y = javascript("1.3+1.2", returns='float')
+print(y)
+
+z = javascript("'string from js'", returns='string')
+print(z)
 
