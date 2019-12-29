6b7020560cce83ad72beccdecba9e19fd18aee0f
raptor <goatman.py@gmail.com>
2019-12-29 10:35:01 +0900

    refactoring list to use std::vector<double> when all items in the list are numbers

---
+++ examples/test_list.py
@@ -0,0 +1,36 @@
+def test():
+	u = [100, 200]
+	print(u)
+	a = []
+	print("append func:", a.append)
+	print("insert func:", a.insert)
+	print("pop func:", a.pop)
+	print("extend func:", a.extend)
+	a.append(1)
+	print(a)
+	a.append(2)
+	print(a)
+	#b = a[:]  ## TODO support slice copy
+	#print(b)
+	print('testing copy vec list')
+	c = list(a)
+	print(c)
+	print('testing index vec list')
+	print( c[0] )
+	print( c[-1] )
+	print('testing vec list multiply by number')
+	print( c * 4 )
+	print('testing vec list extend')
+	print('extend func: ', c.extend)
+	c.extend([99,100])
+	print(c)
+	print('--------------------------')
+	x = ['a', 'b', 'c']
+	print(x)
+	print(x[0])
+	print(x[-1])
+	print(x * 4 )
+	x.extend([99,100])
+	print(x)
+
+test()
