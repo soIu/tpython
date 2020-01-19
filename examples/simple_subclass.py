3f5d7ef63e2b51463545f1400842f4fb5a2310a7
raptor <goatman.py@gmail.com>
2020-01-19 13:37:35 +0900

    fixed subclasses, deprecated __new__

--- examples/simple_subclass.py
+++ examples/simple_subclass.py
@@ -11,16 +11,28 @@ class Bar( Foo ):
 		return self.y
 
 def test():
+	print('making Foo')
 	f = Foo( 1, 'foo')
+	print('made Foo')
+	print(f)
+	print('testing f.x')
 	print(f.x)
+	print('testing f.get_x()')
 	print(f.get_x())
+	print('testing f.y')
 	print(f.y)
-
+	print('making Bar')
 	b = Bar( 10, 'bar')
+	print('made Bar')
+	print(b)
+	print('testing b.x')
 	print(b.x)
+	print('testing b.get_x()')
 	print(b.get_x())
+	print('testing b.y')
 	print(b.y)
+	print('testing b.get_y()')
 	print(b.get_y())
-
+	print('OK')
 
 test()
