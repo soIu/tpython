c56c3f62b44b40b39f417fbdba15af33bc0d5c99
raptor <goatman.py@gmail.com>
2020-02-04 23:53:31 +0900

    ode hello world now runs in interpreter https://gitlab.com/hartsantler/tpythonpp/issues/23

--- examples/hello_ode.py
+++ examples/hello_ode.py
@@ -7,7 +7,7 @@ def test():
 	m.setSphere( 2500.0, 0.05)
 	b.setMass( m )
 	b.setPosition( 0, 2, 0 )
-	b.addForce( 0, 200, 0 )
+	b.addForce( vec3(0, 200, 0) )
 	time = 0.0
 	dt = 0.04
 	while time < 2.0:
